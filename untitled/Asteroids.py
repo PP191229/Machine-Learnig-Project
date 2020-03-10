b6429d93585477a4dc1eff7c919476ec7ae1ac43
PP191229 <PP191229@falmouth.ac.uk>
2020-03-10 13:37:10 +0000

    fdfdfds

--- untitled/Asteroids.py
+++ untitled/Asteroids.py
@@ -2,52 +2,43 @@ import sys
 import pygame
 from pygame.locals import *
 
+pygame.init()
 
-class Asteroid:
-    def __init__(self, image, height, speed):
-        self.speed = speed
-        self.image = image
-        self.pos = image.get_rect().move(0, height)
-
-    def move(self):
-        self.pos = self.image.get_rect().move(0, self.speed)
-        if self.pos.right > 500:
-            self.pos.left = 100
-
-
-class Ship(pygame.sprite.Sprite):
-    def __init__(self, side):
-        pygame.sprite.Sprite.__init__(self)
-        self.speed = 10
-        self.image, self.rect = pygame.image.load('spaceship.png').convert()
-        selfScreen = pygame.display.get_surface()
-        self.area = selfScreen.get_rect()
-
-
-screen = pygame.display.set_mode((900, 900))
-
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-background.fill((0, 0, 0))
+screen = pygame.display.set_mode((800, 600))
 
 asteroid = pygame.image.load('asteroid.png').convert()
-ship = pygame.image.load('spaceship.png').convert()
+# Player
+shipImag = pygame.image.load('battleship.png')
+shipX = 360
+shipY = 420
+shipX_movement = 0
 
-screen.blit(background, (0, 0))
-objects = []
 
-for x in range(10):
-    o = Asteroid(asteroid, x * 400, x)
-    objects.append(o)
+def ship(x, y):
+    screen.blit(shipImag, (x, y))
+
 
 while 1:
     for event in pygame.event.get():
-        if event.type in (QUIT, KEYDOWN):
+        if event.type in (QUIT, K_ESCAPE):
             sys.exit()
-    for o in objects:
-        screen.blit(background, o.pos, o.pos)
-    for o in objects:
-        o.move()
-        screen.blit(o.image, o.pos)
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                shipX_movement = -0.3
+            if event.key == pygame.K_RIGHT:
+                shipX_movement = 0.3
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                shipX_movement = 0
+
+    shipX += shipX_movement
+
+    if shipX <= 0:
+        shipX = 0
+    elif shipX >= 736:
+        shipX = 736
+
+    ship(shipX, shipY)
     pygame.display.update()
-    pygame.time.delay(100)
