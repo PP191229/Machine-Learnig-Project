8d68a6ce88902169c0d8d6d3815d97bb32082a2a
PP191229 <PP191229@falmouth.ac.uk>
2020-03-11 11:47:49 +0000

    feer

--- untitled/Asteroids.py
+++ untitled/Asteroids.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 import sys
 
 pygame.init()
@@ -18,6 +19,28 @@ shipX = 360
 shipY = 420
 shipX_movement = 0
 
+# Bomb
+bombImag = pygame.image.load('bomb.png')
+bombX = 360
+bombY = 484
+
+
+def collision(bombX, bombY, asteroidX, asteroidY):
+    distance = math.sqrt((math.pow(bombX - asteroidX, 2)) + (math.pow(bombY - asteroidY, 2)))
+    if distance < 20:
+        return True
+    else:
+        return False
+
+
+def shipDistance(shipX, asteroidX):
+    distance = math.fabs(asteroidX - shipX)
+    print(distance)
+    if distance < 20:
+        return True
+    else:
+        return False
+
 
 def ship(x, y):
     screen.blit(shipImag, (x, y))
@@ -27,34 +50,64 @@ def asteroid(x, y):
     screen.blit(asteroidImag, (x, y))
 
 
+def bomb(x, y):
+    screen.blit(bombImag, (x, y))
+
+
+bombIsPlanted = True
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                shipX_movement = -0.3
-            if event.key == pygame.K_RIGHT:
-                shipX_movement = 0.3
+    # if event.type == pygame.KEYDOWN:
+    #     if event.key == pygame.K_LEFT:
+    #         shipX_movement = -0.3
+    #    if event.key == pygame.K_RIGHT:
+    #         shipX_movement = 0.3
+    #     if event.key == pygame.K_SPACE:
+    #         bombX = shipX
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                shipX_movement = 0
+    Distance = shipDistance(shipX, asteroidX)
+    if Distance:
+        bombX = asteroidX
+
+    if bombX == asteroidX:
+        if asteroidX > 400:
+            shipX_movement = -0.3
+
+        elif asteroidX <= 400:
+            shipX_movement = 0.3
+        shipX += shipX_movement
+    elif bombX != asteroidX:
+        if shipX > asteroidX:
+            shipX_movement = -0.3
+        else:
+            shipX_movement = 0.3
+        shipX += shipX_movement
+
+    Collided = collision(bombX, bombY, asteroidX, asteroidY)
+    if Collided:
+        asteroidX = random.randint(10, 736)
+        asteroidY = -5
+        print("Collided")
 
     asteroidY += asteroid_movement
     if asteroidY >= 800:
-        asteroidX = random.randint(10, 790)
+        asteroidX = random.randint(64, 736)
         asteroidY = -5
 
-    shipX += shipX_movement
+    # shipX += shipX_movement
 
     if shipX <= 0:
         shipX = 0
     elif shipX >= 736:
         shipX = 736
 
+    screen.fill((0, 0, 0))
+    bomb(bombX, bombY)
     asteroid(asteroidX, asteroidY)
     ship(shipX, shipY)
     pygame.display.update()
